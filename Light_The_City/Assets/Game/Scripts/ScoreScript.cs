247e013698041c688b6db7c0655fc1c8856ab09d
joseedsonamorim <37834114+joseedsonamorim@users.noreply.github.com>
2018-06-01 21:52:58 -0300

    updates

---
+++ Light_The_City/Assets/Game/Scripts/ScoreScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+	public static int scoreValue = 0;
+	Text Score;
+
+	void Start () 
+	{
+		Score = GetComponent<Text> ();
+	}
+	
+	void Update () 
+	{
+		Score.text = "Score; " + scoreValue;
+	}
+}
